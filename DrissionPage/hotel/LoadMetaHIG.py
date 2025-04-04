aec55290de66c6d4543475b587d9b9ddc42521d3
jiangxun <jxoucjj@126.com>
2025-04-04 09:57:45 +0800

    正则解决数据url有时是v1，有时是v3的问题

--- DrissionPage/hotel/LoadMetaHIG.py
+++ DrissionPage/hotel/LoadMetaHIG.py
@@ -32,8 +32,10 @@ def main():
     db = HotelDatabase()
 
     try:
-        # 监听网络中所有满足的url请求 
-        page.listen.start('https://apis.ihg.com.cn/hotels/v3/profiles/') 
+        # 监听网络中所有满足的url请求。因为IHG的酒店详情页url有时是https://apis.ihg.com.cn/hotels/v3/profiles/，有时是https://apis.ihg.com.cn/hotels/v1/profiles/，有时是https://apis.ihg.com.cn/hotels/*/profiles/，所以需要监听所有满足的url请求。
+        # https://github.com/Russel-JX/DrissionPage/blob/master/docs_en/ChromiumPage/network_listener.md  监听网络数据
+        page.listen.start(targets='https://apis.ihg.com.cn/hotels/.*/profiles/', is_regex=True)
+
         # 只监听网络中1个URL
         # page.listen.start('https://apis.ihg.com.cn/hotels/v3/profiles/NKGRS/details?fieldset=brandInfo,location,reviews,profile,address,parking,media,policies,facilities,badges,stripes,room,renovations,tax,marketing,greenEngage,renovationAlerts.active&brandCode=HIEX&ihg-language=zh-cn') 
         # 浏览器中的URL。注：浏览器中的URL，可对应多个网络的url
@@ -41,21 +43,36 @@ def main():
         page.get(url)
 
         #遍历所有具体酒店细节的请求结果
-        for packet in page.listen.steps():
+        """
+         将响应数据的 "hotelContent.hotelCode"、"hotelContent.brandInfo.brandCode"、"hotelContent.brandInfo.brandName"、"hotelContent.profile.name"、
+        "hotelContent.profile.latLong.longitude"、"hotelContent.profile.latLong.latitude"、"hotelContent.address.translatedMainAddress.line1.value"、
+        "hotelContent.profile.entityOpenDate"属性值取出，使用现有的HotelDatabase.py文件中的insert_data方法存到数据库的hotel表中，分别对应hotel表的
+        hotelcode、brandcode、enname、name、longitude、latitude、address、startyear列中。
+        {'hotelcode': 'NKGRS', 'brandcode': 'HIEX', 'enname': 'Holiday Inn Express', 'name': '南京滨江智选假日酒店', 'longitude': '118.73766', 'latitude': '32.09012', 'address': '江苏省南京市鼓楼区公共路18号', 'startyear': '2024-08-13'}
+        """
+        # packets = page.listen.steps()
+        # logging.info(f"捕获到总请求数：{len(packets)}")
+        packets = list(page.listen.steps())  # 将生成器转换为列表
+        logging.info(f"捕获到总请求数：{len(packets)}")
+        for packet in packets:
             logging.info(f"捕获到请求：{packet.url}")
-            hotel = packet.response.body['hotelContent'][0]
-            hotel_data = {
-                'hotelcode': hotel.get('hotelCode'),
-                'brandcode': hotel.get('brandInfo').get('brandCode'),
-                'enname': hotel.get('brandInfo').get('brandName'),
-                'name': hotel.get('profile').get('name')[0].get('value'),
-                'longitude': hotel.get('profile').get('latLong').get('longitude'),
-                'latitude': hotel.get('profile').get('latLong').get('latitude'),
-                'address': hotel.get('address').get('translatedMainAddress').get('line1')[0].get('value'),
-                'startyear': hotel.get('profile').get('entityOpenDate')
-                }
-            db.insert_data('hotel', hotel_data)
-            logging.info(f"有效数据：{hotel_data}")
+            # hotel = packet.response.body['hotelContent'][0]
+            # hotel_data = {
+            #     'groupcode': hotel.get('IHG'),
+            #     'groupname': hotel.get('洲际'),
+            #     'brandname': hotel.get('brandInfo').get('brandName'),
+            #     'hotelcode': hotel.get('hotelCode'),
+            #     'brandcode': hotel.get('brandInfo').get('brandCode'),
+            #     'enname': hotel.get('brandInfo').get('brandName'),
+            #     'name': hotel.get('profile').get('name')[0].get('value'),
+            #     'longitude': hotel.get('profile').get('latLong').get('longitude'),
+            #     'latitude': hotel.get('profile').get('latLong').get('latitude'),
+            #     'address': hotel.get('address').get('translatedMainAddress').get('line1')[0].get('value'),
+            #     'city': hotel.get('address').get('translatedMainAddress').get('city')[0].get('value'),
+            #     'startyear': hotel.get('profile').get('entityOpenDate')
+            #     }
+            # db.insert_data('hotel', hotel_data)
+            # logging.info(f"有效数据：{hotel_data}")
     except Exception as e:
         print(f"运行过程中发生错误：{e}")
     finally:
